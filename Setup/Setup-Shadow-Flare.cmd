15697de9a3923a1f4f79f82689bf0fd63679b919
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-20 17:35:41 -0500

    Update Setup-Shadow-Flare.cmd

--- Setup/Setup-Shadow-Flare.cmd
+++ Setup/Setup-Shadow-Flare.cmd
@@ -1,18 +1,5 @@
-@echo off
-goto check_Permissions
+echo OFF
 
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-    
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
-    ) else (
-        echo Failure: Current permissions inadequate.
-    )
-    
-    pause >nul
-    
 set "IPADDRESS=8.8.8.8"
 
 :TestNetworkConnection
