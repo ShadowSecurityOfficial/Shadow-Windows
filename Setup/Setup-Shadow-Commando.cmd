b7d88452f5d4e46ce25a90a27bd42f647724b114
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-20 17:21:19 -0500

    Update Setup-Shadow-Commando.cmd

--- Setup/Setup-Shadow-Commando.cmd
+++ Setup/Setup-Shadow-Commando.cmd
@@ -1,17 +1,28 @@
-@echo off
+@rem ----[ This code block detects if the script is being running with admin PRIVILEGES If it isn't it pauses and then quits]-------
+echo OFF
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+) ELSE (
+   echo ######## ########  ########   #######  ########  
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ######   ########  ########  ##     ## ########  
+   echo ##       ##   ##   ##   ##   ##     ## ##   ##   
+   echo ##       ##    ##  ##    ##  ##     ## ##    ##  
+   echo ######## ##     ## ##     ##  #######  ##     ## 
+   echo.
+   echo.
+   echo ####### ERROR: ADMINISTRATOR PRIVILEGES REQUIRED #########
+   echo This script must be run as administrator to work properly!  
+   echo If you're seeing this after clicking on a start menu icon, then right click on the shortcut and select "Run As Administrator".
+   echo ##########################################################
+   echo.
+   PAUSE
+   EXIT /B 1
+)
+@echo ON
 
-goto check_Permissions
-
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-    
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
-    ) else (
-        echo Failure: Current permissions inadequate.
-    )
-        
 set "IPADDRESS=8.8.8.8"
 
 :TestNetworkConnection
