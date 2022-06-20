57495241ab130a0a4aa7aec077cdeaa0a06d44a0
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-20 17:10:16 -0500

    Update Setup-Shadow-Commando.cmd

--- Setup/Setup-Shadow-Commando.cmd
+++ Setup/Setup-Shadow-Commando.cmd
@@ -1,3 +1,18 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+    
+    pause >nul
+    
 set "IPADDRESS=8.8.8.8"
 
 :TestNetworkConnection
