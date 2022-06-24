6c9e8c5efeef5bb905bd1d4b0bf8480d93dad41c
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 15:08:18 -0500

    Update Fetch-All.ps1

--- Setup/Fetch-All.ps1
+++ Setup/Fetch-All.ps1
@@ -1,3 +1,15 @@
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
+
+while (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
+    echo "No internet connection found, retrying."
+    Start-Sleep -Seconds 30   
+}
+
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/PortableGit-2.36.1-64-bit.7z.exe -o git.exe
 Invoke-WebRequest https://www.rarlab.com/rar/winrar-x64-611.exe -o winrar.exe
 Invoke-WebRequest https://www.7-zip.org/a/7z2200-x64.exe -o 7zip.exe
