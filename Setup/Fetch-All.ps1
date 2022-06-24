2d470bb921066ea10714d19ef8697b976dde821d
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 12:30:22 -0500

    Create Fetch-All.ps1

---
+++ Setup/Fetch-All.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/PortableGit-2.36.1-64-bit.7z.exe
+Invoke-WebRequest https://www.rarlab.com/rar/winrar-x64-611.exe
+Invoke-WebRequest https://www.7-zip.org/a/7z2200-x64.exe
+Invoke-WebRequest https://github.com/Atlas-OS/atlas-releases/releases/download/20H2-v0.5.2/Atlas_v0.5.2_Faceit_Edition.iso
+Invoke-WebRequest https://downloads.ntlite.com/files/NTLite_setup_x64.exe
