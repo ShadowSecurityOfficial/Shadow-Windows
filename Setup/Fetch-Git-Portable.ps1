53ceda70b8200e19881f1b67bda5e1b243f620c1
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 12:50:49 -0500

    Update Fetch-Git-Portable.ps1

--- Setup/Fetch-Git-Portable.ps1
+++ Setup/Fetch-Git-Portable.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/PortableGit-2.36.1-64-bit.7z.exe
+Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/PortableGit-2.36.1-64-bit.7z.exe -o 7zip.exe
