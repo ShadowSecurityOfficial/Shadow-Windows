33faba5915089f95a4fe503f4147f574fb99e4de
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 13:40:05 -0500

    Create Shadow-Windows-Flare.ps1

---
+++ Source/Desktop/Shadow/Shadow-Windows-Flare.ps1
@@ -0,0 +1,38 @@
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
+
+while (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
+    echo "No internet connection found, retrying."
+    Start-Sleep -Seconds 30   
+}
+
+Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe -o git.exe
+.\git.exe /SILENT
+git clone https://github.com/mandiant/flare-vm.git
+cd flare-vm
+Unblock-File .\install.ps1
+Set-ExecutionPolicy Unrestricted -f
+.\install.ps1
+
+$MyWallpaper="%HomeDrive%\Windows\Web\Wallpaper.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+[Win32.Wallpaper]::SetWallpaper($MyWallpaper)
