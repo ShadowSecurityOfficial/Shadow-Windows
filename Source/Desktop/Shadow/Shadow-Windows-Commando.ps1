9a8f92d245ab7c4e8a454b2bebe6be9624d5b31d
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 13:00:24 -0500

    Update Shadow-Windows-Commando.ps1

--- Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
+++ Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
@@ -1,3 +1,10 @@
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
+
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe -o git.exe
 .\git.exe /SILENT
 git clone https://github.com/mandiant/commando-vm.git
