e4961eae60a5e03ec2310ba31456438ded5c739d
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 16:20:16 -0500

    Update Shadow-Windows-Commando.ps1

--- Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
+++ Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
@@ -13,10 +13,9 @@ while (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe -o git.exe
 Start-Process .\git.exe /SILENT -NoNewWindow -Wait
 git clone https://github.com/mandiant/commando-vm.git
-cd commando-vm
 Unblock-File .\commando-vm\install.ps1
 Set-ExecutionPolicy Unrestricted -f
-Start-Process .\flare-vm\install.ps1 -NoNewWindow -Wait
+Start-Process .\commando-vm\install.ps1 -NoNewWindow -Wait
 
 $MyWallpaper="%HomeDrive%\Windows\Web\Wallpaper.jpg"
 $code = @' 
