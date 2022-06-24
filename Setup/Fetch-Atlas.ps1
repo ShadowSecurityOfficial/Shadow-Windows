c3d72353853dd1bc62abaad874bc65adb3aeaa78
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 12:50:21 -0500

    Update Fetch-Atlas.ps1

--- Setup/Fetch-Atlas.ps1
+++ Setup/Fetch-Atlas.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest https://github.com/Atlas-OS/atlas-releases/releases/download/20H2-v0.5.2/Atlas_v0.5.2_Faceit_Edition.iso
+Invoke-WebRequest https://github.com/Atlas-OS/atlas-releases/releases/download/20H2-v0.5.2/Atlas_v0.5.2_Faceit_Edition.iso -o Atlas.iso
