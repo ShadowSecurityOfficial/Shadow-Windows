601222e8c8f6fce4cb1fd8c3973ae5ff8b270b61
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 15:09:32 -0500

    Update Fetch-NTlite.ps1

--- Setup/Fetch-NTlite.ps1
+++ Setup/Fetch-NTlite.ps1
@@ -1,1 +1,13 @@
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
+
+while (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
+    echo "No internet connection found, retrying."
+    Start-Sleep -Seconds 30   
+}
+
 Invoke-WebRequest https://downloads.ntlite.com/files/NTLite_setup_x64.exe -o NTlite.exe
