6e50f6010fc56efa6fbf116113d954c5ed3f2f2a
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 16:16:30 -0500

    Update Shadow-Windows-Flare.ps1

--- Source/Desktop/Shadow/Shadow-Windows-Flare.ps1
+++ Source/Desktop/Shadow/Shadow-Windows-Flare.ps1
@@ -11,12 +11,12 @@ while (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
 }
 
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe -o git.exe
-& .\git.exe /SILENT
+Start-Process .\git.exe /SILENT -NoNewWindow -Wait
 git clone https://github.com/mandiant/flare-vm.git
 cd flare-vm
 Unblock-File .\install.ps1
 Set-ExecutionPolicy Unrestricted -f
-& .\install.ps1
+Start-Process .\install.ps1 -NoNewWindow -Wait
 
 $MyWallpaper="%HomeDrive%\Windows\Web\Wallpaper.jpg"
 $code = @' 
