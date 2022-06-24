d8c1e785e5c6b5639d5a0a1ec1de1db574bc4ae3
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 12:56:53 -0500

    Update Shadow-Windows-Commando.ps1

--- Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
+++ Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
@@ -1,2 +1,7 @@
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe -o git.exe
 .\git.exe /SILENT
+git clone https://github.com/mandiant/commando-vm.git
+cd commando-vm
+Unblock-File .\install.ps1
+Set-ExecutionPolicy Unrestricted -f
+.\install.ps1
