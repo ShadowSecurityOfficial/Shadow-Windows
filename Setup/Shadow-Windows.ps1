823231f02fc8879b4e80ac5df0688ced739716a9
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-24 15:55:57 -0500

    Update Shadow-Windows.ps1

--- Setup/Shadow-Windows.ps1
+++ Setup/Shadow-Windows.ps1
@@ -1,4 +1,9 @@
-Remove-Item $PSCommandPath -Force 
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
 
 $MyWallpaper="%HomeDrive%\Windows\Web\Wallpaper.jpg"
 $code = @' 
@@ -18,3 +23,5 @@ namespace Win32{
 
 add-type $code 
 [Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+
+Remove-Item $PSCommandPath -Force
