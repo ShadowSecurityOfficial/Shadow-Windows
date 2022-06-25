2f01665b76d0696971239eca92359ae7543dc22c
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-25 16:45:59 -0500

    Update Shadow-Windows.bat

--- Setup/Shadow-Windows.bat
+++ Setup/Shadow-Windows.bat
@@ -1,2 +1,2 @@
-powershell.exe -File %HomeDrive%\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Shadow-Windows.ps1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%HomeDrive%\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Shadow-Windows.ps1'"
 (goto) 2>nul & del "%~f0"
