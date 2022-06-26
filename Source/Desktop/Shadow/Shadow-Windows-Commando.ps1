8f3a98b3c8b8dcd35de8aca309faad1f5fa7969e
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-25 19:58:15 -0500

    Update Shadow-Windows-Commando.ps1

--- Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
+++ Source/Desktop/Shadow/Shadow-Windows-Commando.ps1
@@ -19,7 +19,7 @@ Remove-Item .\commando-vm -Recurse -Force
 git clone https://github.com/mandiant/commando-vm.git .\commando-vm
 Unblock-File .\commando-vm\install.ps1
 Set-ExecutionPolicy Unrestricted -f
-& .\commando-vm\install.ps1
+-NoProfile -Command & .\commando-vm\install.ps1 $nochecks = $true
 
 $MyWallpaper="%HomeDrive%\Windows\Web\Wallpaper.jpg"
 $code = @' 
