12c75d2c9e1f6def8483301b9a373ac78f395615
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-06-28 20:34:06 -0500

    Update Shadow-Windows.ps1

--- Setup/Shadow-Windows.ps1
+++ Setup/Shadow-Windows.ps1
@@ -26,4 +26,5 @@ namespace Win32{
 add-type $code 
 [Win32.Wallpaper]::SetWallpaper($MyWallpaper)
 
-choco; New-Item -Path $Profile -Type File -Force -Value "pwsh.exe -NoExit -Command 'Set-Alias -Name Get-WMIObject -Value Get-CIMInstance'"
+choco; New-Item -Path $Profile -Type File -Force -Value "pwsh.exe -NoExit -Command 'Set-Alias -Name Get-WMIObject -Value Get-CIMInstance'"; Copy-Item  -Path %HomeDrive%\ProgramData\Microsoft\Windows\Start Menu\Programs\FireEyeVM.Common.psm1 -Destination %HomeDrive%\ProgramData\FEVM\FireEyeVM.Common\FireEyeVM.Common.psm1 -Recurse -Force
+
