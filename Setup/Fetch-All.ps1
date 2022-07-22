82a4b56c89904eee72b836bf025ace297f2c1759
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-07-22 15:32:24 -0500

    Update Fetch-All.ps1

--- Setup/Fetch-All.ps1
+++ Setup/Fetch-All.ps1
@@ -13,6 +13,6 @@ while (-not (Test-Connection 8.8.8.8 -Count 1 -Quiet)) {
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/PortableGit-2.36.1-64-bit.7z.exe -o git.exe
 Invoke-WebRequest https://www.rarlab.com/rar/winrar-x64-611.exe -o winrar.exe
 Invoke-WebRequest https://www.7-zip.org/a/7z2200-x64.exe -o 7zip.exe
-Invoke-WebRequest https://wpc.epubg691.workers.dev/0:/OS/WIN11.MacOS.Monterey.Edition.x64.iso -o windows.iso
+Invoke-WebRequest https://server2.epubg691.workers.dev/0:/WIN10.MacOS.Big.Sur.Edition.1909.x64.[The.World.Of.PC].rar -o windows_iso_password:_WPC_BigSur.rar
 Invoke-WebRequest https://downloads.ntlite.com/files/NTLite_setup_x64.exe -o ntlite.exe
 Invoke-WebRequest https://github.com/namazso/OpenHashTab/releases/download/v3.0.2/OpenHashTab_setup.exe -o openhashtab.exe
